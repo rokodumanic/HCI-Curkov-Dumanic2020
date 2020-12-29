3d8fc732ff141953b560dc22fb7a63d0c6bdd996
Bratelion <ntzurkovy@gmail.com>
2020-12-29 17:57:38 +0100

    Added favicon and site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Helmet from "react-helmet"
 
 import ContactBar from "../components/ContactBar"
 import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <>
+    <Helmet><title>OPG Tabar</title></Helmet>
+
     <ContactBar />
     <Footer />
   </>
