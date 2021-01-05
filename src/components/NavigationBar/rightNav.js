21ec78d154319c0b894874b7e1b20da306382947
Ante PICS <>
2021-01-05 16:00:46 +0100

    Toggle Menu responsive

--- src/components/NavigationBar/rightNav.js
+++ src/components/NavigationBar/rightNav.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { navs as navTabs } from '../../constants/const'
 import { Link } from 'gatsby'
 
+import styles from './style.module.css'
+
+
 const RightNav = () => {
   return (
-    <div>
+    <ul className={styles.links}>
         {navTabs.map(({ tab, to }) => (
-            <Link to={to}>
-                <span>{tab}</span>
-            </Link>)
+            <li>
+                <Link to={to}>
+                    <span>> {tab}</span>
+                </Link>
+            </li>)
         )}
-    </div>
+    </ul>
   )
 }
 
