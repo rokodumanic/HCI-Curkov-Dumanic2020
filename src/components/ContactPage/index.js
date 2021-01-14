66839fdfd3d2b1fbb05dc43f5a7aa6ab2ec19d8a
Ante PICS <ntzurkovy@gmail.com>
2021-01-14 13:54:33 +0100

    Google API secrets

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import styles from './style.module.css'
 
+const API_URL = process.env.GATSBY_GOOGLE_URL
+const API_KEY = process.env.GATSBY_GOOGLE_API
+const API_QUERY = "Kaštel+Novi"
+
 const IndexPage = () => {
   return (
     <div className={styles.container}>
-      <iframe className={styles.map} src="https://www.google.com/maps/embed/v1/place?key=AIzaSyADGAutWLrWQWAOjOfpCHa2Up20BLNMvO8
-          &q=Kaštel+Novi" allowfullscreen>
+      <iframe className={styles.map} src= {API_URL.replace("$key", API_KEY).replace("$query", API_QUERY)} allowfullscreen>
         </iframe>
     </div>
   )
--- src/constants/photos.js
+++ src/constants/photos.js
@@ -1,7 +1,7 @@
 export const photos = Array.from({length:20},()=> 
     (
         {
-            'src': 'https://source.unsplash.com/collection/256460?'+ Math.ceil(Math.random()*200),
+            'src': 'https://source.unsplash.com/collection/256460?'+ Math.ceil(Math.random()*1000),
             'width': Math.floor(Math.random()*4) + 2,
             'height': Math.floor(Math.random()*4) + 2
         }
