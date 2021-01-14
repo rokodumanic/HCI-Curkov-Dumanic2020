9026b1842521db1a3056743ae8d841222a2f128a
Roko PICS <dumanicroko@gmail.com>
2021-01-14 16:45:40 +0100

    Added css

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -5,12 +5,13 @@ import { news } from "../../constants/news";
 const News = () =>  {
     return (
         <div>
-            {news.map(({ src, title, spoiler}, index) => {
+            {news.map(({ src, title, date, spoiler}, index) => {
                     return (
                         <div className={styles.container}>
                         <img className={styles.src} src={src} title="news picture"/>
                             <div className={styles.content}>
                                 <span className={styles.title}>{title}</span>
+                                <span className={styles.date}>{date}</span>
                                 <p className={styles.spoiler}>{spoiler}</p>
                             </div>
                         </div>
