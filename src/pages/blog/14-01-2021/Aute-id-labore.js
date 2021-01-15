7dfa8e0fcebd88e5bad24bfc049dac0e404166c7
Roko PICS <global>
2021-01-15 16:42:14 +0100

    Added all news pages

--- src/pages/blog/14-01-2021/Aute-id-labore.js
+++ src/pages/blog/14-01-2021/Aute-id-labore.js
@@ -2,18 +2,21 @@ import React from "react"
 import StandardPage from "../../../components/templates/StandardPage"
 import {news} from "../../../constants/news"
 
-import styles from "../styles.module.css"
+import style from "../style.module.css"
+
 
 
 const IndexPage = props => {
   const item = news.find(i=>i.to === props.location.pathname);
   return (
     <StandardPage props={props}>
-    <div>
-        <img  src={item.src} title="news picture"/>
-        <span >{item.title}</span>
-        <span >{item.date}</span>
-        <p >{item.text}</p>
+    <div className={style.body}>
+        <img className={style.image} src={item.src} title="news picture"/>
+        <div className={style.titleContainer}>
+          <span className={style.title}>{item.title}</span>
+          <span className={style.date}>{item.date}</span>
+        </div>
+        <p className={style.text}>{item.text}</p>
     </div>
 
     </StandardPage>
