0e02a7e84bdbc9078359694ebd14f91d29ebbed1
Roko PICS <dumanicroko@gmail.com>
2021-01-18 14:52:31 +0100

    Added products to Proizvodi page

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import styles from './style.module.css'
-
-const Product = () => (
-    <div>
-        
-    </div>
-)
+import { meso, mlijeko, sirevi, usluge } from "../../constants/products";
+ 
+const Product = () => {
+    return (
+        <div>
+            {meso.map(({ category }, index) => {
+                return (
+                    <div className={styles.container}>
+                        <span>{category}</span>
+                        {meso.map(({ name, price }, index) => {
+                            return(
+                                <div>
+                                    <div className={styles.pricing}>
+                                        <span className={styles.name}>{name}</span>
+                                        <span className={styles.price}>{price}</span>
+                                    </div>
+                                </div>
+                                )
+                            }
+                        )}
+                    </div>
+                    )
+                }
+            )
+            }
+        </div>
+    )
+}
 
 export default Product
--- src/constants/products.js
+++ src/constants/products.js
@@ -1,16 +1,22 @@
-export const navs = [
+export const meso = [
     {category:'Meso', name:'Govedina', price:'0kn/kg'},
     {category:'Meso', name:'Teletina', price:'0kn/kg'}, 
     {category:'Meso', name:'Junetina', price:'0kn/kg'}, 
     {category:'Meso', name:'Kozletina', price:'0kn/kg'},
-    {category:'Meso', name:'Piletina', price:'0kn/kg'},
+    {category:'Meso', name:'Piletina', price:'0kn/kg'}
+]
+export const mlijeko =[
     {category:'Mlijeko', name:'Kravlje', price:'0kn/l'},
-    {category:'Mlijeko', name:'Kozje', price:'0kn/l'},
+    {category:'Mlijeko', name:'Kozje', price:'0kn/l'}
+]
+export const sirevi =[
     {category:'Sirevi', name:'Kravlji mladi', price:'0kn/kg'},
     {category:'Sirevi', name:'Kravlji tvrdi', price:'0kn/kg'},
     {category:'Sirevi', name:'Kozji mladi', price:'0kn/kg'},
     {category:'Sirevi', name:'Kozji tvrdi', price:'0kn/kg'},
-    {category:'Sirevi', name:'Skuta', price:'0kn/kg'},
+    {category:'Sirevi', name:'Skuta', price:'0kn/kg'}
+]
+export const usluge =[
     {category:'Ostale usluge', name:'Traktor', price:'0kn/h'},
     {category:'Ostale usluge', name:'Kosilica', price:'0kn/h'},
     {category:'Ostale usluge', name:'Bager', price:'0kn/h'},
