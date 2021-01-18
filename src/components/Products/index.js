31e908f4a8c554c04ec29e838acdbc40bb8df81c
Roko PICS <dumanicroko@gmail.com>
2021-01-18 16:26:28 +0100

    Styling ProductPage

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -4,8 +4,9 @@ import { meso, mlijeko, sirevi, usluge } from "../../constants/products";
  
 const Product = () => {
     return (
-        <div className={styles.container}>
-            <span className={styles.category}>Meso</span>
+        <>
+        <div className={[styles.container, styles.cat1].join(" ")}>
+            <span className={styles.category}>meso</span>
             {meso.map(({ name, price }, index) => {
                 return(
                     <div>
@@ -19,6 +20,52 @@ const Product = () => {
                 }
             )}
         </div>
+        <div className={[styles.container, styles.cat2].join(" ")}>
+            <span className={styles.category}>mlijeko</span>
+            {mlijeko.map(({ name, price }, index) => {
+                return(
+                    <div>
+                        <div className={styles.pricing}>
+                            <ul className={styles.name}>{name}</ul>
+                            <div className={styles.dots}></div>
+                            <ul className={styles.price}>{price}</ul>
+                        </div>
+                    </div>
+                    )
+                }
+            )}
+        </div>
+        <div className={[styles.container, styles.cat3].join(" ")}>
+            <span className={styles.category}>sirevi</span>
+            {sirevi.map(({ name, price }, index) => {
+                return(
+                    <div>
+                        <div className={styles.pricing}>
+                            <ul className={styles.name}>{name}</ul>
+                            <div className={styles.dots}></div>
+                            <ul className={styles.price}>{price}</ul>
+                        </div>
+                    </div>
+                    )
+                }
+            )}
+        </div>
+        <div className={[styles.container, styles.cat4].join(" ")}>
+            <span className={styles.category}>usluge</span>
+            {usluge.map(({ name, price }, index) => {
+                return(
+                    <div>
+                        <div className={styles.pricing}>
+                            <ul className={styles.name}>{name}</ul>
+                            <div className={styles.dots}></div>
+                            <ul className={styles.price}>{price}</ul>
+                        </div>
+                    </div>
+                    )
+                }
+            )}
+        </div>
+        </>
     )
 }
 export default Product
--- src/constants/products.js
+++ src/constants/products.js
@@ -1,25 +1,25 @@
 export const meso = [
-    {category:'Meso', name:'Govedina', price:'0kn/kg'},
-    {category:'Meso', name:'Teletina', price:'0kn/kg'}, 
-    {category:'Meso', name:'Junetina', price:'0kn/kg'}, 
-    {category:'Meso', name:'Kozletina', price:'0kn/kg'},
-    {category:'Meso', name:'Piletina', price:'0kn/kg'}
+    {category:'Meso', name:'Govedina', price:'0 kn/kg'},
+    {category:'Meso', name:'Teletina', price:'0 kn/kg'}, 
+    {category:'Meso', name:'Junetina', price:'0 kn/kg'}, 
+    {category:'Meso', name:'Kozletina', price:'0 kn/kg'},
+    {category:'Meso', name:'Piletina', price:'0 kn/kg'}
 ]
 export const mlijeko =[
-    {category:'Mlijeko', name:'Kravlje', price:'0kn/l'},
-    {category:'Mlijeko', name:'Kozje', price:'0kn/l'}
+    {category:'Mlijeko', name:'Kravlje', price:'0 kn/l'},
+    {category:'Mlijeko', name:'Kozje', price:'0 kn/l'}
 ]
 export const sirevi =[
-    {category:'Sirevi', name:'Kravlji mladi', price:'0kn/kg'},
-    {category:'Sirevi', name:'Kravlji tvrdi', price:'0kn/kg'},
-    {category:'Sirevi', name:'Kozji mladi', price:'0kn/kg'},
-    {category:'Sirevi', name:'Kozji tvrdi', price:'0kn/kg'},
-    {category:'Sirevi', name:'Skuta', price:'0kn/kg'}
+    {category:'Sirevi', name:'Kravlji mladi', price:'0 kn/kg'},
+    {category:'Sirevi', name:'Kravlji tvrdi', price:'0 kn/kg'},
+    {category:'Sirevi', name:'Kozji mladi', price:'0 kn/kg'},
+    {category:'Sirevi', name:'Kozji tvrdi', price:'0 kn/kg'},
+    {category:'Sirevi', name:'Skuta', price:'0 kn/kg'}
 ]
 export const usluge =[
-    {category:'Ostale usluge', name:'Traktor', price:'0kn/h'},
-    {category:'Ostale usluge', name:'Kosilica', price:'0kn/h'},
-    {category:'Ostale usluge', name:'Bager', price:'0kn/h'},
-    {category:'Ostale usluge', name:'Balirka', price:'0kn/h'},
-    {category:'Ostale usluge', name:'Malčer', price:'0kn/h'}
+    {category:'Ostale usluge', name:'Traktor', price:'0 kn/sat'},
+    {category:'Ostale usluge', name:'Kosilica', price:'0 kn/sat'},
+    {category:'Ostale usluge', name:'Bager', price:'0 kn/sat'},
+    {category:'Ostale usluge', name:'Balirka', price:'0 kn/sat'},
+    {category:'Ostale usluge', name:'Malčer', price:'0 kn/sat'}
 ]
