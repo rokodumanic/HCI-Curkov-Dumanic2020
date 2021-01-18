8668e1102017bb5b1b15f884ccd7da23525cd0e3
Roko PICS <dumanicroko@gmail.com>
2021-01-18 13:21:34 +0100

    Aproducts to constants

---
+++ src/components/Products/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Product = () => (
+    <div>
+        
+    </div>
+)
+
+export default Product
---
+++ src/constants/products.js
@@ -0,0 +1,19 @@
+export const navs = [
+    {category:'Meso', name:'Govedina', price:'0kn/kg'},
+    {category:'Meso', name:'Teletina', price:'0kn/kg'}, 
+    {category:'Meso', name:'Junetina', price:'0kn/kg'}, 
+    {category:'Meso', name:'Kozletina', price:'0kn/kg'},
+    {category:'Meso', name:'Piletina', price:'0kn/kg'},
+    {category:'Mlijeko', name:'Kravlje', price:'0kn/l'},
+    {category:'Mlijeko', name:'Kozje', price:'0kn/l'},
+    {category:'Sirevi', name:'Kravlji mladi', price:'0kn/kg'},
+    {category:'Sirevi', name:'Kravlji tvrdi', price:'0kn/kg'},
+    {category:'Sirevi', name:'Kozji mladi', price:'0kn/kg'},
+    {category:'Sirevi', name:'Kozji tvrdi', price:'0kn/kg'},
+    {category:'Sirevi', name:'Skuta', price:'0kn/kg'},
+    {category:'Ostale usluge', name:'Traktor', price:'0kn/h'},
+    {category:'Ostale usluge', name:'Kosilica', price:'0kn/h'},
+    {category:'Ostale usluge', name:'Bager', price:'0kn/h'},
+    {category:'Ostale usluge', name:'Balirka', price:'0kn/h'},
+    {category:'Ostale usluge', name:'Malčer', price:'0kn/h'}
+]
