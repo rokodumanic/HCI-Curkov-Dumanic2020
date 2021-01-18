59ec1b0f045897ea17a79a3fef330590c03da569
Roko PICS <dumanicroko@gmail.com>
2021-01-18 16:53:03 +0100

    Minor changes to BlogPost

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import StandardPage from "../../components/templates/StandardPage"
 import {news} from "../../constants/news"
 
-import style from "./style.module.css"
+import styles from "./style.module.css"
 
 
 
@@ -10,13 +10,13 @@ const IndexPage = (props) => {
   const item = news.find(i=>i.to === window.location.pathname);
   return (
     <StandardPage props={props}>
-    <div className={style.body}>
-         <img className={style.image} src={item.src} alt="" title="news picture"/>
-        <div className={style.titleContainer}>
-          <span className={style.title}>{item.title}</span>
-          <span className={style.date}>{item.date}</span>
+    <div className={styles.body}>
+         <img className={styles.image} src={item.src} alt="" title="news picture"/>
+        <div className={styles.titleContainer}>
+          <span className={styles.title}>{item.title}</span>
+          <span className={styles.date}>{item.date}</span>
         </div>
-        <p className={style.text}>{item.text}</p> 
+        <p className={styles.text}>{item.text}</p> 
     </div>
 
     </StandardPage>
