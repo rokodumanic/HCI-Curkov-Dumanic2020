2a279b64d97866134190539f26323a7bca8095b0
Roko PICS <dumanicroko@gmail.com>
2021-01-21 10:34:52 +0100

    Added more news

--- src/constants/news.js
+++ src/constants/news.js
@@ -46,4 +46,145 @@ export const news = [
     text:
       "Sunt non duis velit ex do fugiat excepteur nisi sit est elit. Elit pariatur do commodo elit duis dolore fugiat nulla consectetur ad. Sit esse pariatur nulla ad amet minim cupidatat ea excepteur magna pariatur id. Esse fugiat non nisi aute sunt aliqua magna enim dolore velit consequat id dolore aute. Do elit minim ut eu occaecat magna cillum ut anim. Duis incididunt et eiusmod ad consectetur pariatur nulla qui in sit sunt cillum. In sint tempor nostrud occaecat id amet.",
   },
+  {
+    src: "https://source.unsplash.com/collection/2320271?6",
+    to: "/blog/09-01-2021/Enim-voluptate-sunt",
+    title:
+      "Enim voluptate sunt consequat est quis quis cillum dolore excepteur exercitation exercitation eu cupidatat dolor.",
+    date: "9. Siječanj, 2021",
+    spoiler: "Amet nostrud magna nostrud consectetur excepteur deserunt non.",
+    text:
+    "Amet nostrud magna nostrud consectetur excepteur deserunt non. Dolor eiusmod proident dolor voluptate do sunt aliquip do. Ex aliquip aliquip eu anim consectetur ad eu. Anim adipisicing sunt in duis non. Reprehenderit ut dolor id anim minim ut nostrud velit id cupidatat sint tempor magna."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?7",
+    to: "/blog/08-01-2021/Est-id-nisi-commodo",
+    title:
+      "Est id nisi commodo exercitation sunt aute occaecat labore qui proident ipsum incididunt quis.",
+    date: "8. Siječanj, 2021",
+    spoiler: "Sint veniam amet anim magna pariatur sit id enim.",
+    text:
+    "Sint veniam amet anim magna pariatur sit id enim. Dolor commodo labore nostrud pariatur mollit. Esse eu ullamco laborum fugiat ipsum consectetur do in veniam labore incididunt est ad."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?8",
+    to: "/blog/08-01-2021/Anim-consectetur",
+    title:
+    "Anim consectetur adipisicing ut qui dolor sit ex non nisi ea in excepteur quis amet.",    
+    date: "8. Siječanj, 2021",
+    spoiler: "Qui adipisicing commodo proident in aliquip non ...",
+    text:
+    "Qui adipisicing commodo proident in aliquip non amet amet officia aute voluptate aliquip. Non laboris ullamco officia non nulla excepteur qui elit enim. Do labore magna occaecat eu tempor exercitation laborum."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?9",
+    to: "/blog/07-01-2021/Ut-cupidatat-laboris",
+    title:
+      "Ut cupidatat laboris duis duis cupidatat veniam et nisi.",
+    date: "7. Siječanj, 2021",
+    spoiler: "Culpa non pariatur occaecat amet nostrud anim amet ...",
+    text:
+    "Culpa non pariatur occaecat amet nostrud anim amet aute magna. Et enim laborum cillum laboris ullamco labore magna ipsum deserunt laboris nulla eiusmod. Aute reprehenderit consectetur cupidatat qui incididunt ea. Pariatur qui in aliquip esse enim. Aute cillum exercitation veniam nulla velit culpa amet reprehenderit excepteur."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?10",
+    to: "/blog/07-01-2021/Cupidatat-amet-esse",
+    title:
+      "Cupidatat amet esse irure minim elit id et.",
+    date: "7. Siječanj, 2021",
+    spoiler: "Sit non ipsum non minim laborum deserunt consectetur ...",
+    text:
+    "Sit non ipsum non minim laborum deserunt consectetur incididunt. Anim nostrud eu minim eu commodo ut voluptate et do adipisicing id. Consectetur cillum exercitation ad aute dolor. Mollit esse anim est ipsum nostrud ipsum eu est tempor duis mollit."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?11",
+    to: "/blog/07-01-2021/Ex-adipisicing-laboris",
+    title:
+      "Ex adipisicing laboris irure eiusmod minim laboris officia ullamco aliquip pariatur amet.",
+    date: "7. Siječanj, 2021",
+    spoiler: "Adipisicing ut non Lorem ea. Elit amet dolor ...",
+    text:
+    "Adipisicing ut non Lorem ea. Elit amet dolor est mollit ex elit est consectetur anim consequat cupidatat occaecat eiusmod consectetur. Velit aliqua do fugiat do anim nulla ad cupidatat mollit nisi fugiat cillum enim enim. Aute in exercitation exercitation exercitation aliquip Lorem ut mollit ea duis duis. Non irure tempor nisi in anim labore irure consectetur tempor. Nulla sit consequat cillum consequat pariatur cupidatat aliquip."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?12",
+    to: "/blog/07-01-2021/Id-Lorem-pariatur",
+    title:
+      "Id Lorem pariatur laborum tempor esse aliquip aliquip sunt.",
+    date: "7. Siječanj, 2021",
+    spoiler: "Laboris officia do elit velit occaecat amet qui ...",
+    text:
+    "Laboris officia do elit velit occaecat amet qui Lorem fugiat in est minim. Sit consequat qui pariatur ullamco nostrud ex aliqua sint culpa amet proident. Ex eu ut officia est eu commodo ut dolore magna id elit velit enim aute. Occaecat qui do nisi occaecat cupidatat eiusmod voluptate Lorem culpa incididunt amet magna. Do sint irure voluptate enim veniam incididunt nulla velit."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?13",
+    to: "/blog/06-01-2021/Elit-aute-ipsum",
+    title:
+      "Elit aute ipsum aliqua ex ullamco.",
+    date: "6. Siječanj, 2021",
+    spoiler: "Sunt non duis velit ex do fugiat excepteur nisi sit ...",
+    text:
+    "Ullamco anim veniam mollit magna culpa culpa. Incididunt aute nisi commodo minim excepteur in fugiat id magna. Anim occaecat nisi exercitation aliqua reprehenderit cillum exercitation et quis magna nisi laboris tempor. Lorem aute ut do quis eiusmod incididunt. Reprehenderit amet ullamco pariatur velit dolore. Nostrud pariatur duis irure officia labore laborum fugiat pariatur veniam in Lorem aliquip."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?14",
+    to: "/blog/06-01-2021/Tempor-quis-aliqua",
+    title:
+      "Tempor quis aliqua quis Lorem eiusmod et irure excepteur consectetur do dolor.",
+    date: "6. Siječanj, 2021",
+    spoiler: "Culpa ipsum Lorem non laborum Lorem sint occaecat.",
+    text:
+    "Culpa ipsum Lorem non laborum Lorem sint occaecat. Tempor duis fugiat ad occaecat ipsum excepteur excepteur ullamco do pariatur ex occaecat labore. Nostrud dolor sint et nisi ea quis minim laboris nostrud do. Duis fugiat cillum ullamco est culpa nostrud eu non cillum reprehenderit amet voluptate ipsum."  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?15",
+    to: "/blog/06-01-2021/Laborum-proident",
+    title:
+      "Laborum proident consectetur duis laboris excepteur.",
+    date: "6. Siječanj, 2021",
+    spoiler: "Labore veniam anim aute fugiat fugiat nisi ...",
+    text:
+      "Labore veniam anim aute fugiat fugiat nisi duis mollit et et. Esse officia duis anim in officia occaecat qui amet. Duis ullamco minim tempor non voluptate officia duis aute consequat. Magna commodo cupidatat voluptate aliqua est culpa laborum eiusmod elit nisi deserunt deserunt. Amet qui non ad fugiat esse aliquip adipisicing esse.",
+  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?16",
+    to: "/blog/05-01-2021/Tempor-exercitation-proident",
+    title:
+      "Tempor exercitation proident exercitation pariatur anim eu reprehenderit elit quis do.",
+    date: "5. Siječanj, 2021",
+    spoiler: "Sunt amet fugiat ullamco id. Ea labore velit ...",
+    text:
+      "Sunt amet fugiat ullamco id. Ea labore velit Lorem duis reprehenderit. Duis mollit et laborum excepteur laborum proident anim est. In sunt excepteur veniam sint quis ea ad adipisicing ullamco magna exercitation deserunt consequat est. Labore Lorem in cupidatat aliquip aute ipsum. Ullamco mollit cupidatat amet laboris est deserunt velit consequat proident. Eu Lorem voluptate amet pariatur laboris proident.",
+  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?17",
+    to: "/blog/05-01-2021/Et-cillum-reprehenderit",
+    title:
+      "Et cillum reprehenderit magna aute dolor ea incididunt tempor.",
+    date: "5. Siječanj, 2021",
+    spoiler: "Amet aliqua velit ea fugiat eu nostrud cillum do mollit.",
+    text:
+      "Amet aliqua velit ea fugiat eu nostrud cillum do mollit. Culpa eu commodo nisi irure amet amet laboris ex mollit eiusmod non tempor sint quis. Consectetur do consectetur commodo quis labore duis exercitation consequat dolor sunt voluptate pariatur ex consectetur. Laborum nisi eiusmod non irure magna occaecat velit quis aliqua aliquip ea eu commodo eiusmod. Duis sunt duis aute proident non mollit fugiat. Reprehenderit nostrud exercitation cillum exercitation pariatur ut mollit exercitation occaecat anim dolore sit fugiat. Adipisicing anim excepteur ipsum aliqua Lorem deserunt fugiat exercitation eiusmod sit commodo enim.",
+  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?18",
+    to: "/blog/05-01-2021/Magna-ipsum-velit",
+    title:
+      "Magna ipsum velit veniam excepteur fugiat sit veniam.",
+    date: "5. Siječanj, 2021",
+    spoiler: "Labore velit nisi ut dolore cillum consequat id.",
+    text:
+      "Labore velit nisi ut dolore cillum consequat id. Ut incididunt commodo non cupidatat eu id fugiat culpa eu deserunt nisi mollit anim. Sint aliqua est consectetur quis fugiat dolore proident est exercitation do exercitation ex sint. Velit in nostrud non occaecat ipsum culpa quis voluptate sunt laboris.",
+  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?19",
+    to: "/blog/04-01-2021/Sint-anim-tempor",
+    title:
+      "Sint anim tempor exercitation commodo deserunt quis reprehenderit aliqua ipsum culpa laborum ad.",
+    date: "4. Siječanj, 2021",
+    spoiler: "Deserunt labore incididunt aute nisi. Qui cupidatat in ...",
+    text:
+      "Deserunt labore incididunt aute nisi. Qui cupidatat in sint duis dolore sit non elit laboris minim voluptate pariatur minim esse. Esse eiusmod non mollit labore voluptate nisi amet incididunt tempor exercitation est. Sit ad do proident ex excepteur.",
+  },
+  {
+    src: "https://source.unsplash.com/collection/2320271?20",
+    to: "/blog/04-01-2021/Quis-excepteur-voluptate",
+    title:
+      "Quis excepteur voluptate esse id culpa.",
+    date: "4. Siječanj, 2021",
+    spoiler: "Deserunt labore incididunt aute nisi. Qui cupidatat in ...",
+    text:
+      "Veniam in consequat nulla ut mollit et nostrud laborum ut aliquip. Velit minim sunt anim fugiat mollit sint ut qui qui. Qui ipsum officia dolore ad quis ad. Quis cupidatat commodo sit ut non cillum in. Officia ea nisi amet nulla sint amet velit excepteur ullamco. Sit laborum eu quis cupidatat. Sunt cupidatat in est eu anim esse ad minim Lorem adipisicing culpa pariatur Lorem.",
+  }
 ]
