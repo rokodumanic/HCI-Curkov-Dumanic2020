d6313145ccd4a14f13ea68261353bfb4f1de913a
Ante PICS <ntzurkovy@gmail.com>
2021-01-21 10:53:01 +0100

    Adding tags search to posts

--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -4,30 +4,50 @@ import { news } from "../../constants/news";
 import { Link } from 'gatsby'
 
 const News = () =>  {
+    let search;
     let item = "";
     if (typeof window !== `undefined`) {
         let params = (new URL(document.location)).searchParams;
         item = params.get('q');
-        console.log(item);
+    }
+
+    if(item.includes("#")){
+        search = news.filter(titles => titles.tag.toLocaleLowerCase().includes(item.toLocaleLowerCase())).map(({ src, to, title, date, spoiler, text}, index) => {
+            return (
+                <Link key={index} to={to} state={{src, title, date, text}}>
+                <div className={styles.container}>
+                <img className={styles.src} src={src} alt="" title="news picture"/>
+                    <div className={styles.content}>
+                        <span className={styles.title}>{title}</span>
+                        <span className={styles.date}>{date}</span>
+                        <p className={styles.spoiler}>{spoiler}</p>
+                    </div>
+                </div>
+                </Link>
+                )
+            }
+        );
+    }
+    else {
+        search = news.filter(titles => titles.title.toLocaleLowerCase().includes(item.toLocaleLowerCase())).map(({ src, to, title, date, spoiler, text}, index) => {
+            return (
+                <Link key={index} to={to} state={{src, title, date, text}}>
+                <div className={styles.container}>
+                <img className={styles.src} src={src} alt="" title="news picture"/>
+                    <div className={styles.content}>
+                        <span className={styles.title}>{title}</span>
+                        <span className={styles.date}>{date}</span>
+                        <p className={styles.spoiler}>{spoiler}</p>
+                    </div>
+                </div>
+                </Link>
+                )
+            }
+        );
     }
     return (
         <div>
-            {news.filter(titles => titles.title.includes(item)).map(({ src, to, title, date, spoiler, text}, index) => {
-                    return (
-                        <Link key={index} to={to} state={{src, title, date, text}}>
-                        <div className={styles.container}>
-                        <img className={styles.src} src={src} alt="" title="news picture"/>
-                            <div className={styles.content}>
-                                <span className={styles.title}>{title}</span>
-                                <span className={styles.date}>{date}</span>
-                                <p className={styles.spoiler}>{spoiler}</p>
-                            </div>
-                        </div>
-                        </Link>
-                        )
-                    }
-                )
-            }
+            {search}
         </div>
     )
 }
